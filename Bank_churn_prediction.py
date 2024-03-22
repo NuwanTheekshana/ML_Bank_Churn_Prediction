986c64f35905928802e9389159be07e1b4084cbd
NuwanTheekshana <nuwantheekshana723@gmail.com>
2024-03-22 11:12:30 +0530

    add project

---
+++ Bank_churn_prediction.py
@@ -0,0 +1,23 @@
+import pickle
+
+model = pickle.load(open('D:\Python\CI Assignment\BC_model.pkl', 'rb'))
+
+CreditScore = int(input('Please enter CreditScore: '))
+Geography = int(input('Please enter Geography (0-France, 1-Germany, 2-Spain): '))
+Gender = int(input('Please enter Gender (0-Female, 1-Male): '))
+Age = int(input('Please enter Age: '))
+Tenure = int(input('Please enter Tenure: '))
+Balance = float(input('Please enter Balance: '))
+NumOfProducts = int(input('Please enter NumOfProducts: '))
+HasCrCard = int(input('Please enter HasCrCard: '))
+IsActiveMember = int(input('Please enter IsActiveMember: '))
+EstimatedSalary = float(input('Please enter EstimatedSalary: '))
+
+pred = model.predict([[CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]])
+
+
+if(pred == 0):
+    print("Exited")
+else:
+    print("Not Exited")
+    
