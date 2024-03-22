824ef16e7314d92a7fd5a7540dea39005378d665
NuwanTheekshana <nuwantheekshana723@gmail.com>
2024-03-22 11:21:47 +0530

    pickle location change

--- Bank_churn_prediction.py
+++ Bank_churn_prediction.py
@@ -1,30 +1,51 @@
+import streamlit as st
 import pickle
 import requests
-
 
 url = 'https://github.com/NuwanTheekshana/ML_Bank_Churn_Prediction/raw/main/BC_model.pkl'
 response = requests.get(url)
 with open('BC_model.pkl', 'wb') as f:
-    	f.write(response.content)
+    f.write(response.content)
 
 model = pickle.load(open('BC_model.pkl', 'rb'))
 
-CreditScore = int(input('Please enter CreditScore: '))
-Geography = int(input('Please enter Geography (0-France, 1-Germany, 2-Spain): '))
-Gender = int(input('Please enter Gender (0-Female, 1-Male): '))
-Age = int(input('Please enter Age: '))
-Tenure = int(input('Please enter Tenure: '))
-Balance = float(input('Please enter Balance: '))
-NumOfProducts = int(input('Please enter NumOfProducts: '))
-HasCrCard = int(input('Please enter HasCrCard: '))
-IsActiveMember = int(input('Please enter IsActiveMember: '))
-EstimatedSalary = float(input('Please enter EstimatedSalary: '))
 
-pred = model.predict([[CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]])
+def predict_churn(CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
+    pred = model.predict([[CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]])
+    return pred
+
+# Streamlit app
+st.title('Bank Churn Prediction')
 
 
-if(pred == 0):
-    print("Exited")
-else:
-    print("Not Exited")
+CreditScore = st.number_input('Credit Score:')
+Geography = st.selectbox('Geography', ['France', 'Germany', 'Spain'])
+Gender = st.selectbox('Gender', ['Female', 'Male'])
+Age = st.number_input('Age:')
+Tenure = st.number_input('Tenure:')
+Balance = st.number_input('Balance:')
+NumOfProducts = st.number_input('Number of Products:', min_value=1, max_value=4, value=1)
+HasCrCard = st.selectbox('Has Credit Card?', ['No', 'Yes'])
+IsActiveMember = st.selectbox('Is Active Member?', ['No', 'Yes'])
+EstimatedSalary = st.number_input('Estimated Salary:')
+
+
+if st.button('Predict'):
+    # Map input values to numerical representation
+    geography_mapping = {'France': 0, 'Germany': 1, 'Spain': 2}
+    gender_mapping = {'Female': 0, 'Male': 1}
+    has_cr_card_mapping = {'No': 0, 'Yes': 1}
+    is_active_member_mapping = {'No': 0, 'Yes': 1}
     
+    Geography = geography_mapping[Geography]
+    Gender = gender_mapping[Gender]
+    HasCrCard = has_cr_card_mapping[HasCrCard]
+    IsActiveMember = is_active_member_mapping[IsActiveMember]
+    
+    pred = predict_churn(CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
+    
+    # Display prediction result
+    if pred == 0:
+        st.write("Exited")
+    else:
+        st.write("Not Exited")
